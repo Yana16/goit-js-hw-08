1b651230030220794cbc0df768b1e53ab31666f0
Yana Vozniuk <yana.vozniuk@ukr.net>
2022-11-07 20:40:56 +0300

    new
    new

--- src/js/01-gallery.js
+++ src/js/01-gallery.js
@@ -2,7 +2,7 @@
 import { galleryItems } from './gallery-items';
 // Change code below this line
 // Описан в документации
-import SimpleLightbox from "Simplelightbox";
+import SimpleLightbox from "simplelightbox";
 import "simplelightbox/dist/simple-lightbox.min.css";
 
 console.log(galleryItems);
