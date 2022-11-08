af0684e64d0e9fe9bd5e170b4a6b2a5605288ffe
Yana Vozniuk <yana.vozniuk@ukr.net>
2022-11-09 02:07:06 +0300

    changes
    changes

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -1,25 +1,19 @@
-import Player from "@vimeo/player";
+import Player from '@vimeo/player';
 import throttle from 'lodash.throttle';
 
-const iframe = document.querySelector("iframe");
-const player = new Vimeo.Player(iframe);
-const STORAGE_KEY = "videoplayer-current-time";
+const CURRENT_TIME = "videoplayer-current-time";
 
-
-localStorage.setItem(STORAGE_KEY, data.seconds);
-const onPlay = throttle((data) =>  {
-    localStorage.setItem(STORAGE_KEY, data.seconds); 
-} , 1000);
-
-player.on('timeupdate', onPlay);
-player.getCurrentTime(STORAGE_KEY).then(function(seconds) {
-    // seconds = the current playback position
-}).catch(function(error) {
-    // an error occurred
-});
-
-player.on('loaded', function() {
-    if(localStorage.getItem(STORAGE_KEY)) {
-        player.setCurrentTime(localStorage.getItem(STORAGE_KEY));
+const iframe = document.querySelector('iframe');
+const player = new Player(iframe);
+player.on('timeupdate', throttle(onPlay, 1000));
+   
+function onPlay ({ seconds }) {
+    localStorage.setItem(CURRENT_TIME, seconds)
+}
+setCurrentTime()
+function setCurrentTime(){
+    if(!localStorage.getItem(CURRENT_TIME)){
+        return
     }
-});
+    player.setCurrentTime(localStorage.getItem(CURRENT_TIME))
+}
